b7e694ea3c0936972b28bb8da20355ff41371a68
Kushal Pisavadia <kushal.p@apple.com>
2025-02-17 20:45:41 +0000

    Configure `hawkeye` to update license headers (#38)
    Adds configuration to format/check license headers with hawkeye.
    
    Also, runs the formatter on the repo.

--- Sources/PklSwiftInternals/Discovery.cpp
+++ Sources/PklSwiftInternals/Discovery.cpp
@@ -1,12 +1,18 @@
-//
-// This source file is part of the Swift.org open source project
-//
-// Copyright (c) 2024 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
-// See https://swift.org/LICENSE.txt for license information
-// See https://swift.org/CONTRIBUTORS.txt for Swift project authors
-//
+/*
+ * Copyright © 2024-2025 Apple Inc. and the Pkl project authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "Discovery.h"
 
